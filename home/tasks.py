c134f34669bc053508abd2cbfd0cd38c002c66e6
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-11-06 19:32:46 +0200

    s

--- home/tasks.py
+++ home/tasks.py
@@ -9,12 +9,16 @@ CHAT_ID = "1028452911"
 def send_telegram_message(text):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {"chat_id": CHAT_ID, "text": text}
-    requests.post(url, data=data)
+    response = requests.post(url, data=data)
+    print("🔹 Telegram Response:", response.status_code, response.text)  # <== هنا هنطبع النتيجة
 
 @shared_task
 def check_monthly_payments():
     today = timezone.now().date()
+    print("📅 Running check_monthly_payments:", today)  # <== نطبع التاريخ
     for sub in Subscriber.objects.all():
+        print("👤 Checking:", sub.name, sub.next_payment_due())  # <== نطبع كل عميل
         if today >= sub.next_payment_due():
             message = f"💰 العميل {sub.name} المفروض يدفع النهارده ({today})"
+            print("🚀 Sending message:", message)
             send_telegram_message(message)
--- project/settings.py
+++ project/settings.py
@@ -82,12 +82,35 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'KlmXIPcESmUdWNLyxpkdWsdExamuppfx',
+        'HOST': 'tramway.proxy.rlwy.net',
+        'PORT': '13731',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':'railway',
+#         "USER": "postgres",
+#         "PASSWORD": "JZnhoDtVVfuyYqqIeMEQCTiONPcSxmSx",
+#         "HOST": "trolley.proxy.rlwy.net",
+#         "PORT": '38941',
+#     }
+# }
 
 
 # Password validation
